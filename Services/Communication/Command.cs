c9df93911298b1a362966f2f296934bc76e9a050
MioBestWaifu <Fenix1603@hotmail.com>
2024-05-04 17:20:15 -0300

    Creating the command class

---
+++ Services/Communication/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maria.Services.Communication
+{
+    public class Command
+    {
+        public List<string>? Prefixes { get; set; }
+        public string Root { get; set; }
+        public List<string>? Options { get; set; }
+        public Dictionary<string,string>? Modifiers { get; set; }
+
+        public Command (string root)
+        {
+            Root = root;
+        }
+    }
+}
