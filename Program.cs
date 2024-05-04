4459c7a46bbf83adcbb252e0c8a8a8d45c234714
MioBestWaifu <Fenix1603@hotmail.com>
2024-05-03 22:17:36 -0300

    setup

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Maria_chan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
