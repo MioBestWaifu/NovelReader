3ee74bdf69e28e5181520978988791d64b0b96ee
MioBestWaifu <Fenix1603@hotmail.com>
2024-05-05 20:43:30 -0300

    Implement client and server

--- Common/Communication/CommandClient.cs
+++ Common/Communication/CommandClient.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Maria.Common.Communication.Commanding;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Maria.Common.Communication
 {
+    //This scheme does not suffice for long-running, multiple comms and will be expanded when needed.
+    //It also is inadequate to handle timing-related prefixes
     public class CommandClient
     {
+        //Adaptive port and inter-device communication may be achieved by simply editing this value.
+        private static string serverUrl = "http://localhost:47100"; 
+        private HttpClient httpClient;
+
+        public CommandClient()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(serverUrl);
+        }
+
+        public void SendCommand(Command command)
+        {
+            httpClient.PostAsync($"/{command.Action}", new StringContent(JsonSerializer.Serialize(command))).RunSynchronously();
+        }
     }
 }
--- Common/Communication/CommandServer.cs
+++ Common/Communication/CommandServer.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using Maria.Common.Communication.Commanding;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Maria.Common.Communication
 {
+    //This is also inadequate. The delegate should return a message. The server should be stoppable and resumable. 
+    //It is not made for the finished application.
     public class CommandServer
     {
+        public delegate void CommandReceived(Command command);
+        public event CommandReceived OnCommandReceived;
+        private HttpListener httpListener;
+
+        public CommandServer()
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:47100/");
+            httpListener.Start();
+            Task.Run(() => Listen());
+        }
+
+        public async Task Listen()
+        {
+            while (true)
+            {
+                HttpListenerContext context = await httpListener.GetContextAsync();
+                RaiseCommandReceived(context);
+            }
+        }
+
+        //This should have exception handling.
+        private void RaiseCommandReceived(HttpListenerContext context)
+        {
+            string requestBody;
+
+            using (Stream body = context.Request.InputStream)
+            {
+                using (StreamReader reader = new StreamReader(body, context.Request.ContentEncoding))
+                {
+                    requestBody = reader.ReadToEnd();
+                    Console.WriteLine($"Received request payload: {requestBody}");
+                }
+            }
+
+            Command command = JsonSerializer.Deserialize<Command>(requestBody);
+
+            OnCommandReceived?.Invoke(command);
+        }
+
     }
 }
