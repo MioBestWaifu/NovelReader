fa36d9a00ce426d88b5495cdf07aabc0dfcaeb2a
MioBestWaifu <Fenix1603@hotmail.com>
2024-05-05 20:49:27 -0300

    Creating Interpreter and registering event listener

---
+++ Services/Communication/Interpreter.cs
@@ -0,0 +1,17 @@
+﻿using Maria.Common.Communication.Commanding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maria.Services.Communication
+{
+    internal class Interpreter
+    {
+        public void ProcessComand (Command command)
+        {
+            Console.WriteLine($"{command}");
+        }
+    }
+}
--- Services/Worker.cs
+++ Services/Worker.cs
@@ -1,12 +1,20 @@
+using Maria.Common.Communication;
+using Maria.Services.Communication;
+
 namespace Maria.Services
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly CommandServer commandServer;
+        private readonly Interpreter interpreter;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            commandServer = new CommandServer();
+            interpreter = new Interpreter();
+            commandServer.OnCommandReceived += interpreter.ProcessComand;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -15,7 +23,7 @@ namespace Maria.Services
             {
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
                 await Task.Delay(1000, stoppingToken);
             }
