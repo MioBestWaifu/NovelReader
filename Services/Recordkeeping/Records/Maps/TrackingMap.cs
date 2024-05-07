1c542de70134b140c8b88dd1c39b9b683637e464
MioBestWaifu <Fenix1603@hotmail.com>
2024-05-06 23:00:40 -0300

    Adding CsvHelper and TrackingMap

---
+++ Services/Recordkeeping/Records/Maps/TrackingMap.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maria.Services.Recordkeeping.Records.Maps
+{
+    internal class TrackingMap : ClassMap<TrackingRecord>
+    {
+        public TrackingMap()
+        {
+            Map(m => m.Name).Index(1).Name("name");
+            Map(m => m.Time).Index(2).Name("time");
+        }
+    }
+}
