561b38e7c82e1d38158b6e24faf7acfaf8d2f4c1
MioBestWaifu <Fenix1603@hotmail.com>
2024-05-08 22:46:53 -0300

    Fixing translator initialization problems

--- Services/Translation/Japanese/JapaneseTranslator.cs
+++ Services/Translation/Japanese/JapaneseTranslator.cs
@@ -5,12 +5,13 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace Maria.Services.Translation.Japanese
 {
     /// <summary>
-    /// After initialized, this becomes a big fucking object because it basically holds multiple dictionaries (the thing, not the class)
+    /// After initialized, this becomes a big fucking object, 600mb big, because it basically holds multiple dictionaries (the thing, not the class)
     /// in memory. This could, and depending on how large it gets should, be memory-optmized by keeping only high-priority
     /// lexeme in memory and searching for others on the files as needed. However, as of implementation memory is an abundant resource, 
     /// while response time is absolutely critical. As such, for now i am sacrifing memory for responsiviness.
@@ -24,7 +25,7 @@ namespace Maria.Services.Translation.Japanese
         private JapaneseTranslator() { }
 
         /// <summary>
-        /// This is expensive as fuck
+        /// This is expensive as fuck, like 50 seconds expensive.
         /// </summary>
         public static void Initialize()
         {
@@ -33,28 +34,54 @@ namespace Maria.Services.Translation.Japanese
 
             Stopwatch stopwatch = Stopwatch.StartNew();
             Instance = new JapaneseTranslator();
-            XDocument jmdict = XDocument.Load(pathToData+ "JMdict_e.xml");
-            IEnumerable<XElement> elements = jmdict.Elements("entry");
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.MaxCharactersFromEntities = 0;
+            settings.MaxCharactersInDocument = 0;
+            settings.DtdProcessing = DtdProcessing.Parse;
+            XmlReader reader = XmlReader.Create(pathToData+ "JMdict_e.xml", settings);
+            XDocument jmdict = XDocument.Load(reader);
+            IEnumerable<XElement> elements = jmdict.Element("JMdict")!.Elements("entry");
+            int amountOfArgumentExceptions = 0;
             foreach (XElement element in elements)
             {
+                //Two important things should be done here:
+                //1 - it should check if the entry is usually in kana. this info is contained in the dict but ignored. if it is, then kana keys should be added irrespective of the kanji keys.
+                //2 - the values should be a list of entries, and the user gets all of them as answers.
                 EdrdgEntry entry = new EdrdgEntry(element);
                 if (entry.KanjiElements != null)
                 {
                     foreach (var kanjiElemnt in entry.KanjiElements)
                     {
-                        Instance.conversionTable.Add(kanjiElemnt.Kanji,entry);
+                        try
+                        {
+                            Instance.conversionTable.Add(kanjiElemnt.Kanji, entry);
+                        } catch (ArgumentException)
+                        {
+                            //This is a very common exception, as many entries have the same kanji. It is not an error.
+                            //It will be dealt with eventually.
+                            amountOfArgumentExceptions++;
+                        }
                     }
                 } else
                 {
                     foreach(var readingElemnt in entry.ReadingElements)
                     {
-                        Instance.conversionTable.Add(readingElemnt.Reading, entry);
+                        try
+                        {
+                            Instance.conversionTable.Add(readingElemnt.Reading, entry);
+                        } catch (ArgumentException)
+                        {
+                            //This is a very common exception, as many entries have the same kana. It is not an error.
+                            //It will be dealt with eventually.
+                            amountOfArgumentExceptions++;
+                        }
                     }
                 }
             }
             stopwatch.Stop();
             Console.WriteLine("Time to load: " + stopwatch.ElapsedMilliseconds);
             Console.WriteLine($"Number of elements: {Instance.conversionTable.Count}");
+            Console.WriteLine($"Number of exceptions: {amountOfArgumentExceptions}");
         }
 
         public static void Dispose()
