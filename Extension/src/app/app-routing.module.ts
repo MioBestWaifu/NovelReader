c59719b82a6e14461441d8b3d5beb75007b39736
MioBestWaifu <Fenix1603@hotmail.com>
2024-05-12 00:16:03 -0300

    Navbar, build setup

--- Extension/src/app/app-routing.module.ts
+++ Extension/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OptionsPageComponent } from './components/pages/options-page/options-page.component';
+import { TranslationsPageComponent } from './components/pages/translations-page/translations-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'options',component:OptionsPageComponent},
+  {path:'translations',component:TranslationsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })],
