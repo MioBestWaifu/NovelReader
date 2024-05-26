60ecbc31502c5eb77ea18b3c4a23925af6e7c2fd
MioBestWaifu <Fenix1603@hotmail.com>
2024-05-26 02:21:48 -0300

    Actually adding reader projects

---
+++ Implementations/NovelReader/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maria.NovelReader
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Implementations/NovelReader/Platforms/Android/MainApplication.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Runtime;
+using Maria.NovelReader;
+
+namespace Maria.NovelReader.Platforms.Android
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(nint handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
