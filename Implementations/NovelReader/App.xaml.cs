ca123ca2527f7c6c4af27f341ba163e10c3e1b88
MioBestWaifu <Fenix1603@hotmail.com>
2024-06-08 14:31:58 -0300

    Catching errors when page inits but breaks

--- Implementations/NovelReader/App.xaml.cs
+++ Implementations/NovelReader/App.xaml.cs
@@ -9,6 +9,12 @@ namespace Maria.NovelReader
             InitializeComponent();
 
             MainPage = new MainPage();
+
+            /*AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                Debug.WriteLine(e.ExceptionObject);
+                Debug.WriteLine(s);
+            };*/
         }
     }
 
