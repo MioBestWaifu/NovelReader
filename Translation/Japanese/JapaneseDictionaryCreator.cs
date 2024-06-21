f66c5ac80638094b8cda843ef0de7611423ae47b
MioBestWaifu <Fenix1603@hotmail.com>
2024-06-21 20:03:46 -0300

    Refactoring DictionaryCreator

--- Translation/Japanese/JapaneseDictionaryCreator.cs
+++ Translation/Japanese/JapaneseDictionaryCreator.cs
@@ -15,108 +15,76 @@ namespace Mio.Translation.Japanese
     /// </summary>
     public static class JapaneseDictionaryCreator
     {
-        //TODO refactor this duplicated logic
-        private static ConcurrentDictionary<string, List<(JmdictEntry, int)>> LoadOriginalJMdict(string pathToJmdict)
+        private static XDocument LoadOriginal(string path)
         {
             XmlReaderSettings settings = new XmlReaderSettings();
             settings.MaxCharactersFromEntities = 0;
             settings.MaxCharactersInDocument = 0;
             settings.DtdProcessing = DtdProcessing.Parse;
-            XmlReader reader = XmlReader.Create(pathToJmdict, settings);
-            XDocument jmdict = XDocument.Load(reader);
+            XmlReader reader = XmlReader.Create(path, settings);
+            return XDocument.Load(reader);
+        }
+
+        private static void AddKanjiAndReadingKeys<T>(T entry, ConcurrentDictionary<string, List<(T, int)>> dict) where T : EdrdgEntry
+        {
+            //Should check if the entry is usually in kana. this info is contained in the dict but ignored. if it is, then kana keys should be added irrespective of the kanji keys.
+            int baseKanjiPriority = 0;
+            if (entry.KanjiElements != null)
+            {
+                foreach (var kanjiElement in entry.KanjiElements)
+                {
+                    //this should take things such as commonality, archaism, etc, into account.
+                    int priority = baseKanjiPriority;
+                    List<(T, int)> entries;
+                    if (!dict.TryGetValue(kanjiElement.Kanji, out entries))
+                    {
+                        entries = new List<(T, int)>();
+                        dict.TryAdd(kanjiElement.Kanji, entries);
+                    }
+                    entries.Add((entry, priority));
+                }
+            }
+            if (entry.ReadingElements != null)
+            {
+                int baseReadingPriority = entry.KanjiElements == null || entry.KanjiElements.Count == 0 ? 0 : 10;
+                foreach (var readingElement in entry.ReadingElements)
+                {
+                    //again, should take things such as commonality, archaism, etc, into account.
+                    int priority = baseReadingPriority;
+                    List<(T, int)> entries;
+                    if (!dict.TryGetValue(readingElement.Reading, out entries))
+                    {
+                        entries = new List<(T, int)>();
+                        dict.TryAdd(readingElement.Reading, entries);
+                    }
+                    entries.Add((entry, priority));
+                }
+            }
+        }
+
+        private static ConcurrentDictionary<string, List<(JmdictEntry, int)>> LoadOriginalJMdict(string pathToJmdict)
+        {
+            XDocument jmdict = LoadOriginal(pathToJmdict);
             IEnumerable<XElement> elements = jmdict.Element("JMdict")!.Elements("entry");
             int argumentExisting = 0;
             ConcurrentDictionary<string, List<(JmdictEntry, int)>> toReturn = [];
             Parallel.ForEach(elements, element =>
             {
-                //Two important things should be done here:
-                //1 - it should check if the entry is usually in kana. this info is contained in the dict but ignored. if it is, then kana keys should be added irrespective of the kanji keys.
-                //2 - the values should be a list of entries, and the user gets all of them as answers.
                 JmdictEntry entry = new JmdictEntry(element);
-                int baseKanjiPriority = 0;
-                if (entry.KanjiElements != null)
-                {
-                    foreach (var kanjiElement in entry.KanjiElements)
-                    {
-                        //this should take things such as commonality, archaism, etc, into account.
-                        int priority = baseKanjiPriority;
-                        List<(JmdictEntry, int)> entries;
-                        if (!toReturn.TryGetValue(kanjiElement.Kanji, out entries))
-                        {
-                            entries = new List<(JmdictEntry, int)>();
-                            toReturn.TryAdd(kanjiElement.Kanji, entries);
-                        }
-                        entries.Add((entry, priority));
-                    }
-                }
-                if (entry.ReadingElements != null)
-                {
-                    int baseReadingPriority = entry.KanjiElements == null || entry.KanjiElements.Count == 0 ? 0 : 10;
-                    foreach (var readingElement in entry.ReadingElements)
-                    {
-                        //again, should take things such as commonality, archaism, etc, into account.
-                        int priority = baseReadingPriority;
-                        List<(JmdictEntry, int)> entries;
-                        if (!toReturn.TryGetValue(readingElement.Reading, out entries))
-                        {
-                            entries = new List<(JmdictEntry, int)>();
-                            toReturn.TryAdd(readingElement.Reading, entries);
-                        }
-                        entries.Add((entry, priority));
-                    }
-                }
+                AddKanjiAndReadingKeys(entry, toReturn);
             });
 
             return toReturn;
         }
         private static ConcurrentDictionary<string, List<(NameEntry, int)>> LoadOriginalJMnedict(string pathToJmnedict)
         {
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.MaxCharactersFromEntities = 0;
-            settings.MaxCharactersInDocument = 0;
-            settings.DtdProcessing = DtdProcessing.Parse;
-            XmlReader reader = XmlReader.Create(pathToJmnedict, settings);
-            XDocument jmnedict = XDocument.Load(reader);
+            XDocument jmnedict = LoadOriginal(pathToJmnedict);
             IEnumerable<XElement> elements = jmnedict.Element("JMnedict")!.Elements("entry");
-            int argumentExisting = 0;
             ConcurrentDictionary<string, List<(NameEntry, int)>> toReturn = [];
             Parallel.ForEach(elements, element =>
             {
-                //Two important things should be done here:
-                //1 - it should check if the entry is usually in kana. this info is contained in the dict but ignored. if it is, then kana keys should be added irrespective of the kanji keys.
                 NameEntry entry = new NameEntry(element);
-                int baseKanjiPriority = 0;
-                if (entry.KanjiElements != null)
-                {
-                    foreach (var kanjiElement in entry.KanjiElements)
-                    {
-                        //this should take things such as commonality, archaism, etc, into account.
-                        int priority = baseKanjiPriority;
-                        List<(NameEntry, int)> entries;
-                        if (!toReturn.TryGetValue(kanjiElement.Kanji, out entries))
-                        {
-                            entries = new List<(NameEntry, int)>();
-                            toReturn.TryAdd(kanjiElement.Kanji, entries);
-                        }
-                        entries.Add((entry, priority));
-                    }
-                }
-                if (entry.ReadingElements != null)
-                {
-                    int baseReadingPriority = entry.KanjiElements == null || entry.KanjiElements.Count == 0 ? 0 : 10;
-                    foreach (var readingElement in entry.ReadingElements)
-                    {
-                        //again, should take things such as commonality, archaism, etc, into account.
-                        int priority = baseReadingPriority;
-                        List<(NameEntry, int)> entries;
-                        if (!toReturn.TryGetValue(readingElement.Reading, out entries))
-                        {
-                            entries = new List<(NameEntry, int)>();
-                            toReturn.TryAdd(readingElement.Reading, entries);
-                        }
-                        entries.Add((entry, priority));
-                    }
-                }
+                AddKanjiAndReadingKeys(entry, toReturn);
                 foreach (var translationElement in entry.TranslationElements)
                 {
                     //Should this have priority too?
@@ -132,97 +100,11 @@ namespace Mio.Translation.Japanese
 
             return toReturn;
         }
-        private static ConcurrentDictionary<string, List<(JmdictEntry, int)>> LoadOriginalKanjidic(string pathToKanjidic)
-        {
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.MaxCharactersFromEntities = 0;
-            settings.MaxCharactersInDocument = 0;
-            settings.DtdProcessing = DtdProcessing.Parse;
-            XmlReader reader = XmlReader.Create(pathToKanjidic, settings);
-            XDocument jmdict = XDocument.Load(reader);
-            IEnumerable<XElement> elements = jmdict.Element("JMdict")!.Elements("entry");
-            int argumentExisting = 0;
-            ConcurrentDictionary<string, List<(JmdictEntry, int)>> toReturn = [];
-            Parallel.ForEach(elements, element =>
-            {
-                //Two important things should be done here:
-                //1 - it should check if the entry is usually in kana. this info is contained in the dict but ignored. if it is, then kana keys should be added irrespective of the kanji keys.
-                //2 - the values should be a list of entries, and the user gets all of them as answers.
-                JmdictEntry entry = new JmdictEntry(element);
-                int baseKanjiPriority = 0;
-                if (entry.KanjiElements != null)
-                {
-                    foreach (var kanjiElement in entry.KanjiElements)
-                    {
-                        //this should take things such as commonality, archaism, etc, into account.
-                        int priority = baseKanjiPriority;
-                        List<(JmdictEntry, int)> entries;
-                        if (!toReturn.TryGetValue(kanjiElement.Kanji, out entries))
-                        {
-                            entries = new List<(JmdictEntry, int)>();
-                            toReturn.TryAdd(kanjiElement.Kanji, entries);
-                        }
-                        entries.Add((entry, priority));
-                    }
-                }
-                if (entry.ReadingElements != null)
-                {
-                    int baseReadingPriority = entry.KanjiElements == null || entry.KanjiElements.Count == 0 ? 0 : 10;
-                    foreach (var readingElement in entry.ReadingElements)
-                    {
-                        //again, should take things such as commonality, archaism, etc, into account.
-                        int priority = baseReadingPriority;
-                        List<(JmdictEntry, int)> entries;
-                        if (!toReturn.TryGetValue(readingElement.Reading, out entries))
-                        {
-                            entries = new List<(JmdictEntry, int)>();
-                            toReturn.TryAdd(readingElement.Reading, entries);
-                        }
-                        entries.Add((entry, priority));
-                    }
-                }
-            });
-
-            return toReturn;
-        }
 
         //Yes, thats a lot of lists. No, it is not a better to use other structure. It represents file > content > index.
-        private static List<List<List<ConversionEntry>>> CreateJmdictHashes(string pathToDict)
+        private static List<List<List<ConversionEntry>>> CreateHashes<T>(ConcurrentDictionary<string,List<(T,int)>> originalDict) where T: EdrdgEntry
         {
             List<List<List<ConversionEntry>>> entriesBrokenByIndex = new List<List<List<ConversionEntry>>>();
-
-            ConcurrentDictionary<string, List<(JmdictEntry, int)>> originalDict = LoadOriginalJMdict(pathToDict);
-            for (int i = 0; i < 256; i++)
-            {
-
-                entriesBrokenByIndex.Add(new List<List<ConversionEntry>>());
-                for (int j = 0; j < 256; j++)
-                {
-                    entriesBrokenByIndex[i].Add(new List<ConversionEntry>());
-                }
-
-            }
-
-            int file = 0;
-            SHA256 sha256 = SHA256.Create();
-            foreach (var entryList in originalDict)
-            {
-                byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(entryList.Key));
-                int number = BitConverter.ToUInt16(hash, 0); // Convert the first two bytes to a number
-                foreach (var entry in entryList.Value)
-                {
-                    entriesBrokenByIndex[number / 256][number % 256].Add(new ConversionEntry(entryList.Key, entry.Item1, entry.Item2));
-                }
-            }
-
-            return entriesBrokenByIndex;
-        }
-
-        private static List<List<List<ConversionEntry>>> CreateJmnedictHashes(string pathToDict)
-        {
-            List<List<List<ConversionEntry>>> entriesBrokenByIndex = new List<List<List<ConversionEntry>>>();
-
-            ConcurrentDictionary<string, List<(NameEntry, int)>> originalDict = LoadOriginalJMnedict(pathToDict);
             for (int i = 0; i < 256; i++)
             {
 
@@ -251,20 +133,15 @@ namespace Mio.Translation.Japanese
 
         private static KanjiEntry[] CreateKanjiEntries(string pathToKanjidict)
         {
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.MaxCharactersFromEntities = 0;
-            settings.MaxCharactersInDocument = 0;
-            settings.DtdProcessing = DtdProcessing.Parse;
-            XmlReader reader = XmlReader.Create(pathToKanjidict, settings);
-            XDocument kanjidict = XDocument.Load(reader);
+            XDocument kanjidict = LoadOriginal(pathToKanjidict);
             IEnumerable<XElement> elements = kanjidict.Element("kanjidic2")!.Elements("character");
-            //The largest possible value for the got from DetermineKanjiNumber is supposed be 79-odd thousand.
+            //The largest possible value got from DetermineKanjiNumber is supposed be 79-odd thousand.
             KanjiEntry[] entries = new KanjiEntry[80000];
             int exceptionCount = 0;
             foreach(XElement element in elements)
             {
                 //There is one kanji here making problem: 𠀋. Something to do with bytes and encoding.
-                //There may be others.
+                //There may be others. Seem to be unusual kanji at the end of kanjidic with incomplete information.
                 try
                 {
                     KanjiEntry entry = new KanjiEntry(element);
@@ -286,7 +163,8 @@ namespace Mio.Translation.Japanese
             switch (dictionary)
             {
                 case EdrdgDictionary.JMdict:
-                    List<List<List<ConversionEntry>>> entriesBrokenByFile = CreateJmdictHashes(pathToOriginal);
+                    List<List<List<ConversionEntry>>> entriesBrokenByFile = 
+                        CreateHashes(LoadOriginalJMdict(pathToOriginal));
                     for (int i = 0; i < entriesBrokenByFile.Count; i++)
                     {
                         byte[] jmdictMsgPack = MessagePackSerializer.Serialize(entriesBrokenByFile[i]);
@@ -294,7 +172,8 @@ namespace Mio.Translation.Japanese
                     }
                     break;
                 case EdrdgDictionary.JMnedict:
-                    List<List<List<ConversionEntry>>> jmnedictEntriesBrokenByFile = CreateJmnedictHashes(pathToOriginal);
+                    List<List<List<ConversionEntry>>> jmnedictEntriesBrokenByFile = 
+                        CreateHashes(LoadOriginalJMnedict(pathToOriginal));
                     for (int i = 0; i < jmnedictEntriesBrokenByFile.Count; i++)
                     {
                         byte[] jmnedictMsgPack = MessagePackSerializer.Serialize(jmnedictEntriesBrokenByFile[i]);
